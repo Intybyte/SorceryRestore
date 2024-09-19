f5f99ecdd6cf5c9729408da971bee0d29177fc28
Intybyte <vaan.testwork@gmail.com>
2024-09-19 17:15:16 +0200

    Add mana ability

---
+++ src/main/kotlin/me/vaan/sorceryskill/auraskills/SorceryManaBlast.kt
@@ -0,0 +1,25 @@
+package me.vaan.sorceryskill.auraskills
+
+import dev.aurelium.auraskills.api.mana.CustomManaAbility
+import dev.aurelium.auraskills.api.registry.NamespacedId
+import me.vaan.sorceryskill.SorceryRestore
+import me.vaan.sorceryskill.utils.Utils
+
+object SorceryManaBlast {
+    val BLAST = CustomManaAbility.builder(NamespacedId.of(Utils.PLUGIN_NAME, "mana_blast"))
+        .displayName("Mana blast")
+        .description("Creates a mana blast that deals {value} damage, 5 second cooldown")
+        .baseValue(0.75)
+        .valuePerLevel(0.75)
+        .baseManaCost(15.0)
+        .manaCostPerLevel(5.0)
+        .baseCooldown(5.0)
+        .unlock(6)
+        .levelUp(6)
+        .build()!!
+
+    fun loadManaAbility() {
+        val reg = SorceryRestore.registry()
+        reg.registerManaAbility(BLAST)
+    }
+}
---
+++ src/main/kotlin/me/vaan/sorceryskill/auraskills/listeners/CastListener.kt
@@ -0,0 +1,6 @@
+package me.vaan.sorceryskill.auraskills.listeners
+
+import org.bukkit.event.Listener
+
+object CastListener : Listener {
+}
